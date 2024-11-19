a4984b641ba575991768f45bfda9f109ff24e333
Morgan Bratt <morgansbratt@gmail.com>
2024-11-18 21:01:04 -0800

    Finished Assignment 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+
 # Register your models here.
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # stop from being able to link to posts from the category admin page
+    exclude = ('posts',)
+
+
+class CategoryInline(admin.TabularInline):
+    model = Post.categories.through
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
